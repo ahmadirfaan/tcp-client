5827d077efedfc00d0ad4a385b72b64789bedc31
Ahmad Irfaan <irfaan.hibatullah@gmail.com>
2023-07-25 20:00:28 +0700

    add tcp client template

---
+++ src/main/java/com/irfaan/tcp/client/TCPClientGateway.java
@@ -0,0 +1,13 @@
+package com.irfaan.tcp.client;
+
+
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.stereotype.Component;
+
+@Component
+@MessagingGateway(defaultRequestChannel = TCPClientConfiguration.TCP_DEFAULT_CHANNEL)
+public interface TCPClientGateway {
+
+    byte[] send(byte[] message);
+
+}
